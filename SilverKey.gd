3b11955ad7da94daeab4f1df539a06e1368aafa9
elbow667 <elbow667@gmail.com>
2024-05-30 19:17:55 -0400

    added items

--- SilverKey.gd
+++ SilverKey.gd
@@ -9,4 +9,4 @@ func _on_body_entered(_body: Node2D) -> void:
 	if Global.silverkey_found == false:
 		Global.silverkey_found = true
 		sprite_2d.queue_free()
-		print("The Silver Key was found!")
+		print("You found the silver key!")
--- double_door.gd
+++ double_door.gd
@@ -4,15 +4,11 @@ extends StaticBody2D
 @onready var double_door: StaticBody2D = $"."
 
 
-
-
-
-
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 		if Global.silverkey_found:
 			print("You have the key to this door!")
 			double_door.queue_free()
 		else:
-			print("This door is locked!")
+			print("This door is locked...")
 			
 			
